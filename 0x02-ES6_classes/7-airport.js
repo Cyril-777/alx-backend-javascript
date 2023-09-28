6014a3c93c6c1af4b9c63db2d74a8735f31730d6
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-09-29 02:07:24 +0300

    task 7 - adding getter and setter

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,7 +4,23 @@ export default class Airport {
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(theName) {
+    this._name = theName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(theCode) {
+    this._code = theCode;
+  }
+
   toString() {
-    return `[object ${this.code}]`;
+    return `[object ${this._code}]`;
   }
 }
