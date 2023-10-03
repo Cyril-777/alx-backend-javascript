002f5e7e2114eeb618009058cf9262ac5d35973b
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-03 18:33:58 +0300

    task 8 - fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   return [...set]
-  .filter((value) => (value !== undefined ? value.startsWith(string) : ''))
-  .map((value) => (value !== undefined ? value.slice(string.length) : ''))
+  .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
+  .map((value) => (value !== undefined ? value.slice(startString.length) : ''))
   .join('-');
 }
