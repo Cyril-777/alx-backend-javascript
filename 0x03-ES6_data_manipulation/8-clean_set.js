500674aa8e0e4435182a39b73265fad35c63aba6
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-10-03 18:27:57 +0300

    slice

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
   const filteredString = [...set].filter((value) => value.startsWith(startString));
-  const cleanString = filteredString.map((value) => value.substring(startString.length)).join('-');
+  const cleanString = filteredString.map((value) => value.slice(startString.length)).join('-');
   return cleanString;
 }
