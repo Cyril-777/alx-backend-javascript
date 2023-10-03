26c92c7b224458d3f8cd52796219bcb5032d733a
Cyril-777 <kyrillosmaher10@gmail.com>
2023-10-03 15:36:28 +0000

    task 8 - eslint fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   return [...set]
-  .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
-  .map((value) => (value !== undefined ? value.slice(startString.length) : ''))
-  .join('-');
+    .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
+    .map((value) => (value !== undefined ? value.slice(startString.length) : ''))
+    .join('-');
 }
