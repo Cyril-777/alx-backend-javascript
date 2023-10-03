3ff4dc505d66ab719a3bacce8f63b91d44e9011f
Cyril-777 <kyrillosmaher10@gmail.com>
2023-10-03 14:12:03 +0000

    eslint fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  const filteredString = [...set].filter(value => value.startsWith(startString));
-  const cleanString = filteredString.map(value => value.substring(startString.length)).join('-');
+  const filteredString = [...set].filter((value) => value.startsWith(startString));
+  const cleanString = filteredString.map((value) => value.substring(startString.length)).join('-');
   return cleanString;
 }
