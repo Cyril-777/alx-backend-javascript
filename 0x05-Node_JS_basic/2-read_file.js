9b650d27998891f848e78338626af4cad7a89e96
Cyril-777 <kyrillosmaher10@gmail.com>
2023-11-20 22:40:27 +0000

    eslint fixed

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,14 +4,15 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
 
-    const rows = data.split('\n').filter(line => line.trim() !== '');
+    const rows = data.split('\n').filter((line) => line.trim() !== '');
 
     const counts = {};
     const lists = {};
 
     const [, ...dataRows] = rows;
 
-    dataRows.forEach(row => {
+    dataRows.forEach((row) => {
+      // eslint-disable-next-line no-unused-vars
       const [firstname, lastname, age, field] = row.split(',');
 
       if (!counts[field]) {
@@ -19,17 +20,18 @@ function countStudents(path) {
         lists[field] = [];
       }
 
+      // eslint-disable-next-line no-plusplus
       counts[field]++;
       lists[field].push(firstname);
     });
 
     console.log(`Number of students: ${dataRows.length}`);
 
-    Object.keys(counts).forEach(field => {
+    Object.keys(counts).forEach((field) => {
       console.log(
-        `Number of students in ${field}: ${counts[field]}. ` +
-        `List: ${lists[field].join(', ')}`
-        );
+        `Number of students in ${field}: ${counts[field]}. `
+        + `List: ${lists[field].join(', ')}`,
+      );
     });
   } catch (error) {
     throw new Error('Cannot load the database');
