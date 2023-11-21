268faca1b3d70605bba06ab7cee833dd02010162
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-22 00:21:13 +0200

    Task 4 - add host and removed new line from resp..

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
+const host = '127.0.0.1';
+
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(1245, host, () => {});
 
 module.exports = app;
