eaf21a91626070e85f09041156634377b4f619f6
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-22 01:26:32 +0200

    Task 5 - update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,40 @@
 const http = require('http');
-const { countStudents } = require('./3-read_file_async');
+const fs = require('fs');
 
 const host = '127.0.0.1';
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(Error('Cannot load the database'));
+      if (data) {
+        const content = data.toString().split('\n');
+        const students = content.filter((item) => item).map((item) => item.split(','));
+        const fields = {};
+        let csStudents = [];
+        let sweStudents = [];
+        students.shift();
+        students.forEach((student) => {
+          if (!fields[student[3]]) fields[student[3]] = [];
+          fields[student[3]].push(student[0]);
+        });
+        console.log(`Number of students: ${students.length}`);
+        for (const field in fields) {
+          if (field === 'CS') {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+            csStudents = fields[field];
+          } else if (field === 'SWE') {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+            sweStudents = fields[field];
+          }
+        }
+        console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      }
+      resolve();
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
