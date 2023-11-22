e280bcc5496de718ba219d290f9aaed33e35788c
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-23 01:12:46 +0200

    Task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with the correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWithExactly('SUM', 100, 20)).to.be.true;
+
+    // Restore the spies
+    calculateNumberSpy.restore();
+    consoleLogSpy.restore();
+  });
+});
