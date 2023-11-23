0168461887f790928725029be9608b03d672a242
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-11-23 17:23:13 +0200

    Task 8 - Fix Test Suit

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,38 +1,14 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', () => {
-  let server;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  before((done) => {
-    server = app.listen(7865, () => {
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
-  });
-
-  after((done) => {
-    server.close(() => {
-      done();
-    });
-  });
-
-  it('Correct status code?', (done) => {
-    request.get('http://localhost:7865', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('Correct result?', (done) => {
-    request.get('http://localhost:7865', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('Other?', (done) => {
-    // Add additional tests here
-    done();
   });
 });
